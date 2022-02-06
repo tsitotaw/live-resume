6b5daf2282018353265bdc0fa5fb57bc5cb71c25
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-02-06 00:26:25 -0600

    minor changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(__dirname + '/dist/live-resume'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/live-resume/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + "/src"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/src/index.html"));
+});
+app.listen(process.env.PORT || 8081);
